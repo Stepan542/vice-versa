6365b685adb305c8d2bc63228e63fab0abb6a90f
Stepan <makarovstepka@gmail.com>
2022-01-06 18:00:22 +0600

    Reversed text feature

--- viceversa/views.py
+++ viceversa/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render 
 
-def about(request):
+def home(request):
 	return render(request, 'home.html')
+
+def reverse(request):
+	user_text = request.GET['usertext']
+	reversed_text = user_text[::-1]
+	all_words = user_text.split()
+	count_of_words = len(all_words)
+	return render(request, 'reverse.html',
+		{'usertext':user_text, 'reversedtext':reversed_text, 'countofwords':count_of_words})
+
+
+ 
